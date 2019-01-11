6c5b792a0c37797a32bb90bdcea7069e583f1413
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-01-11 11:16:28 +0100

    Initial commit

---
+++ src/main/java/marioTemomaker1/controlers/MainController.java
@@ -0,0 +1,20 @@
+package marioTemomaker1.controlers;
+
+import marioTemomaker1.views.MenuView;
+
+public class MainController {
+    private MenuView menuView;
+
+    public MainController(){
+        menuView = new MenuView();
+    }
+
+
+    public void startApp(){
+        do{
+
+        }while(true);
+
+    }
+
+}
---
+++ src/main/java/marioTemomaker1/views/MenuView.java
@@ -0,0 +1,19 @@
+package marioTemomaker1.views;
+
+import java.util.Scanner;
+
+public class MenuView {
+    private Scanner scanner;
+
+    public MenuView(){
+        scanner = new Scanner(System.in);
+    }
+    public String getCity(){
+        printString("podaj nazwę miasta:");
+        return scanner.nextLine();
+    }
+
+    private void printString(String s) {
+        System.out.println(s);
+    }
+}
